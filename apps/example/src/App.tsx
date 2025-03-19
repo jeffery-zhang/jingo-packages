ade19ada7b61b2d577fb3f2f3b625301d9c1d094
YX/ZJ <ZJ@yx.com>
2025-03-19 15:37:26 +0800

    feat: push example project

--- apps/example/src/App.tsx
+++ apps/example/src/App.tsx
@@ -1,11 +1,37 @@
-import { bar } from '@packages/bar'
-import { foo } from '@packages/foo'
+import { useEffect, useRef, useState } from 'react'
+
+import { AwaitTimer, IAwaitTimer } from '@jingo/await-timer'
 
 function App() {
-  foo()
-  bar()
+  const [count, setCount] = useState<number>(0)
+  const timer = useRef<IAwaitTimer | null>(null)
 
-  return <div>pnpm-monorepo-boilerplate</div>
+  const loopCallback = (): Promise<void> => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        setCount(prev => prev + 1)
+        resolve()
+      }, 1000)
+    })
+  }
+
+  useEffect(() => {
+    if (!timer.current) {
+      timer.current = new AwaitTimer(loopCallback, 1000, {
+        immediate: true,
+        autoStart: false,
+      })
+      timer.current.start()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (count >= 10) {
+      timer.current?.stop()
+    }
+  }, [count])
+
+  return <div>example: {count}</div>
 }
 
 export default App
